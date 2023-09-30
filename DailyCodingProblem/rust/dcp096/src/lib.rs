5404a6f67da2ef88a7f4c7ab91ac7986bad3960a
Stephen Doyle <stephendoyle75@gmail.com>
2023-09-30 22:48:49 +0100

    Rust solution for DCP #096

---
+++ DailyCodingProblem/rust/dcp096/src/lib.rs
@@ -0,0 +1,37 @@
+/*
+This problem was asked by Microsoft.
+
+Given a number in the form of a list of digits, return all possible permutations.
+
+For example, given [1,2,3],
+return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+*/
+
+use permutator::heap_permutation;
+
+pub fn permutations(data: &mut [i32]) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    heap_permutation(data, |p| {
+        result.push(p.to_vec());
+    });
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_permutations() {
+        let mut data = vec![1, 2, 3];
+        let mut expected = vec![
+            vec![1,2,3],
+            vec![1,3,2],
+            vec![2,1,3],
+            vec![2,3,1],
+            vec![3,1,2],
+            vec![3,2,1]];
+        let mut result = permutations(&mut data);
+        assert_eq!(result.sort(), expected.sort());
+    }
+}
