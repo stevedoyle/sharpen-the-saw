abaa9ccd1d8e9515ca170cf043457787f12ff911
Stephen Doyle <stephen.doyle@intel.com>
2023-12-08 10:40:05 +0000

    AoC2023: Day1 Part 2 solution

--- AoC/2023/day01/src/main.rs
+++ AoC/2023/day01/src/main.rs
@@ -1,3 +1,4 @@
+use log::debug;
 use regex::Regex;
 use std::error::Error;
 use std::fs;
@@ -29,35 +30,35 @@ fn extract_calib_value_digits(line: &str) -> u32 {
 }
 
 fn extract_calib_value(line: &str) -> u32 {
-    let re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").unwrap();
-    // let matches: Vec<_> = re.find_iter(line).map(|m| m.as_str()).collect();
-    let matches: Vec<_> = re.find_iter(line).collect();
-    println!("{matches:?}");
-    let first = matches
-        .iter()
-        .min_by(|x, y| x.start().cmp(&y.start()))
-        .unwrap()
-        .as_str();
-    let last = matches
-        .iter()
-        .max_by(|x, y| x.end().cmp(&y.end()))
-        .unwrap()
-        .as_str();
-    println!("String: {line}, first: {:?}, last: {:?}", first, last,);
-    (str_to_digit(Some(first)) * 10) + str_to_digit(Some(last))
+    // The Regex crated doesn't support overlapping matches, so finding the last digit in a string
+    // can be problematic. For example, a string that ends with 'oneight' will return 1 rather than
+    // 8 when using the Regex crate.
+    let pattern = r"one|two|three|four|five|six|seven|eight|nine|\d";
+    let re = Regex::new(pattern).unwrap();
+    let first = str_to_digit(re.find_iter(line).map(|m| m.as_str()).next());
+
+    // One approach to compensate for this is to reverse the string and the pattern. Not very
+    // efficient but it does the job. A future rev is needed to improve this.
+    let rev_pattern = r"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d";
+    let re = Regex::new(rev_pattern).unwrap();
+    let rev_line: String = line.chars().rev().collect();
+    let last = str_to_digit(re.find_iter(&rev_line).map(|m| m.as_str()).next());
+
+    debug!("String: {line}, first: {:?}, last: {:?}", first, last,);
+    (first * 10) + last
 }
 
 fn str_to_digit(digit: Option<&str>) -> u32 {
     match digit {
-        Some("one") | Some("1") => 1,
-        Some("two") | Some("2") => 2,
-        Some("three") | Some("3") => 3,
-        Some("four") | Some("4") => 4,
-        Some("five") | Some("5") => 5,
-        Some("six") | Some("6") => 6,
-        Some("seven") | Some("7") => 7,
-        Some("eight") | Some("8") => 8,
-        Some("nine") | Some("9") => 9,
+        Some("one") | Some("eno") | Some("1") => 1,
+        Some("two") | Some("owt") | Some("2") => 2,
+        Some("three") | Some("eerht") | Some("3") => 3,
+        Some("four") | Some("ruof") | Some("4") => 4,
+        Some("five") | Some("evif") | Some("5") => 5,
+        Some("six") | Some("xis") | Some("6") => 6,
+        Some("seven") | Some("neves") | Some("7") => 7,
+        Some("eight") | Some("thgie") | Some("8") => 8,
+        Some("nine") | Some("enin") | Some("9") => 9,
         _ => 0,
     }
 }
@@ -65,9 +66,9 @@ fn str_to_digit(digit: Option<&str>) -> u32 {
 fn main() -> Result<(), Box<dyn Error>> {
     let text = fs::read_to_string("input.txt")?;
     let sum = calibration_sum_digits(&text);
-    println!("Calibration sum using only digits: {sum}");
+    println!("Part 1: Calibration sum using only digits: {sum}");
     let sum = calibration_sum(&text);
-    println!("Calibration sum: {sum}");
+    println!("Part 2: Calibration sum: {sum}");
     Ok(())
 }
 
@@ -85,13 +86,13 @@ mod tests {
 
     #[test]
     fn test_line_parse() {
-        // assert_eq!(extract_calib_value("two1nine"), 29);
-        // assert_eq!(extract_calib_value("eightwothree"), 83);
-        // assert_eq!(extract_calib_value("abcone2threexyz"), 13);
-        // assert_eq!(extract_calib_value("xtwone3four"), 24);
-        // assert_eq!(extract_calib_value("4nineeightseven2"), 42);
-        // assert_eq!(extract_calib_value("zoneight234"), 14);
-        // assert_eq!(extract_calib_value("7pqrstsixteen"), 76);
+        assert_eq!(extract_calib_value("two1nine"), 29);
+        assert_eq!(extract_calib_value("eightwothree"), 83);
+        assert_eq!(extract_calib_value("abcone2threexyz"), 13);
+        assert_eq!(extract_calib_value("xtwone3four"), 24);
+        assert_eq!(extract_calib_value("4nineeightseven2"), 42);
+        assert_eq!(extract_calib_value("zoneight234"), 14);
+        assert_eq!(extract_calib_value("7pqrstsixteen"), 76);
         assert_eq!(
             extract_calib_value("ggdone3nbmsthreefourninefiveoneightpr"),
             18
