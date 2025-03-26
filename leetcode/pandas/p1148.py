6a6c12f5c66e082f037aefd13059cb4b59143dcc
Stephen Doyle <stephendoyle75@gmail.com>
2025-03-26 22:16:12 +0000

    (LC Pandas) Add #1148

---
+++ leetcode/pandas/p1148.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[views["author_id"] == views["viewer_id"]]
+    df = df.drop_duplicates(subset=["author_id"])
+    df = df[["author_id"]].rename(columns={"author_id": "id"})
+    df.sort_values("id", inplace=True)
+    return df
+
+
+class TestArticleViews:
+    def test_article_views(self):
+        data = [
+            [1, 3, 5, "2019-08-01"],
+            [1, 3, 6, "2019-08-02"],
+            [2, 7, 7, "2019-08-01"],
+            [2, 7, 6, "2019-08-02"],
+            [4, 7, 1, "2019-07-22"],
+            [3, 4, 4, "2019-07-21"],
+            [3, 4, 4, "2019-07-21"],
+        ]
+
+        views = pd.DataFrame(
+            data, columns=["article_id", "author_id", "viewer_id", "view_date"]
+        ).astype(
+            {
+                "article_id": "Int64",
+                "author_id": "Int64",
+                "viewer_id": "Int64",
+                "view_date": "datetime64[ns]",
+            }
+        )
+
+        df = article_views(views)
+        assert df.shape == (2, 1)
+        assert df.columns == ["id"]
+        print(df)
+        assert df.id.to_list() == [4, 7]
